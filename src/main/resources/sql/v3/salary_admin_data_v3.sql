1891b76cb987ad5f8519b602e7aad0d00bfc7e9b
AliceZhang <huarong290@gmail.com>
2026-03-29 17:09:05 +0800

    salary 薪酬系统v3 基础代码

--- src/main/resources/sql/v3/salary_admin_data_v3.sql
+++ src/main/resources/sql/v3/salary_admin_data_v3.sql
@@ -28,12 +28,19 @@ INSERT INTO `sys_dict_item` (`dict_type_code`, `dict_item_value`, `dict_item_lab
                                                                                                            ('salary_item_sub_type', 'INC_BASE', '基本工资', 10),
                                                                                                            ('salary_item_sub_type', 'INC_ALLOWANCE', '岗位津贴', 20),
                                                                                                            ('salary_item_sub_type', 'INC_OVERTIME', '加班工资', 30),
-                                                                                                           ('salary_item_sub_type', 'INC_BONUS', '绩效奖金', 40);
+                                                                                                           ('salary_item_sub_type', 'INC_ATTENDANCE', '全勤奖', 35),
+                                                                                                           ('salary_item_sub_type', 'INC_BONUS', '绩效奖金', 40),
+                                                                                                           ('salary_item_sub_type', 'INC_YEAR_END', '年终奖', 50),
+                                                                                                           ('salary_item_sub_type', 'INC_SUBSIDY', '补贴', 60),
+                                                                                                           ('salary_item_sub_type', 'INC_FESTIVAL', '节日礼金', 70),
+                                                                                                           ('salary_item_sub_type', 'INC_OTHER', '其他收入', 80);
 
 -- 扣款类细分
 INSERT INTO `sys_dict_item` (`dict_type_code`, `dict_item_value`, `dict_item_label`, `dict_item_sort`) VALUES
                                                                                                            ('salary_item_sub_type', 'DED_ABSENT', '缺勤扣款', 100),
                                                                                                            ('salary_item_sub_type', 'DED_LATE', '迟到早退', 110),
+                                                                                                           ('salary_item_sub_type', 'DED_FINE', '罚款', 115),
+                                                                                                           ('salary_item_sub_type', 'DED_LOAN', '借款扣还', 118),
                                                                                                            ('salary_item_sub_type', 'DED_OTHER', '其他扣款', 120);
 
 -- 统筹与税费 (涉及 PHP 多币种计算的关键项)
@@ -41,9 +48,19 @@ INSERT INTO `sys_dict_item` (`dict_type_code`, `dict_item_value`, `dict_item_lab
                                                                                                            ('salary_item_sub_type', 'TAX_INCOME', '个人所得税', 200),
                                                                                                            ('salary_item_sub_type', 'SI_PENSION', '养老保险(个人)', 210),
                                                                                                            ('salary_item_sub_type', 'SI_MED', '医疗保险(个人)', 220),
-                                                                                                           ('salary_item_sub_type', 'PHP_SSS', 'SSS (菲律宾社保)', 230);
+                                                                                                           ('salary_item_sub_type', 'SI_UNEMPLOYMENT', '失业保险(个人)', 225),
+                                                                                                           ('salary_item_sub_type', 'SI_HOUSING', '住房公积金(个人)', 230),
+                                                                                                           ('salary_item_sub_type', 'PHP_SSS', 'SSS (菲律宾社保)', 240),
+                                                                                                           ('salary_item_sub_type', 'TAX_LOCAL', '地方税', 250);
 
-
+INSERT INTO `sys_dict_item` (`dict_type_code`, `dict_item_value`, `dict_item_label`, `dict_item_sort`) VALUES
+                                                                                                           ('salary_item_sub_type', 'ER_PENSION', '养老保险(公司缴纳)', 300),
+                                                                                                           ('salary_item_sub_type', 'ER_MED', '医疗保险(公司缴纳)', 310),
+                                                                                                           ('salary_item_sub_type', 'ER_HOUSING', '住房公积金(公司缴纳)', 320),
+                                                                                                           ('salary_item_sub_type', 'ER_VISA', '签证费用', 330),
+                                                                                                           ('salary_item_sub_type', 'ER_TRAVEL', '差旅费', 340),
+                                                                                                           ('salary_item_sub_type', 'ER_INSURANCE', '商业保险', 350),
+                                                                                                           ('salary_item_sub_type', 'ER_OTHER', '其他公司支出', 360);
 
 -- 1. 薪资管理 (父级目录)
 INSERT INTO `sys_menu`
